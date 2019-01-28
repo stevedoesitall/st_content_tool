a5ed798c587861b0f200ee9a7417d41337c7289f
Steve Giordano <stephenagiordano@gmail.com>
2019-01-27 23:09:48 -0500

    Oops

--- scripts/server.js
+++ scripts/server.js
@@ -3,12 +3,12 @@ const port = process.env.PORT || 3000;
 const express = require("express");
 const body_parser = require("body-parser");
 const path = require("path");
-// const creds = require("./creds.json");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const dir = path.join(__dirname, "../");
 
+// const creds = require("./creds.json");
 // const api_key = process.env.api_key || creds.api_key;
 // const api_secret = process.env.api_secret || creds.api_secret;
 
@@ -24,105 +24,7 @@ app.post("/server", function(req, res) {
     const sailthru = require("sailthru-client").createSailthruClient(api_key, api_secret);
     let success_count = 0;
 
-    if (req.body.id == "import") {
-        const all_content = req.body.data;
-        all_content.forEach(content => {
-            if (content.tags) {
-            content.tags = content.tags.split(",");
-                console.log(content.tags);
-                content.tags.forEach(tag => {
-                    if (tag.indexOf("|") != -1) {
-                        tag = tag.replace(/\|/g, ",");
-                        tag = tag.replace(" ", "");
-                        console.log(tag);
-                        content.tags = tag.split(",");
-                    }
-                })
-            }
-
-            if (content.inventory) {
-                content.inventory = parseInt(content.inventory);
-                if (content.inventory == NaN) {
-                    delete content.inventory;
-                }
-            }
-
-            else {
-                delete content.inventory;
-            }
-
-            if (content.location) {
-                content.location = content.location.split(",");
-                    content.location.forEach(location => {
-                        if (location.indexOf("|") != -1) {
-                            location = location.replace(/\|/g, ",");
-                            location = location.replace(" ", "");
-                            content.location = location.split(",");
-                            if (content.location.length > 2) {
-                                delete content.location;
-                            }
-                        }
-                        else {
-                            delete content.location;
-                        }
-                    })
-                }
-
-            else {
-                delete content.location;
-            }
-
-            if (content.price) {
-                content.price = parseInt(content.price);
-            }
-
-            if (content.thumb || content.full) {
-                content.images = {};
-                if (content.full) {
-                    content.images.full = {};
-                        content.images.full.url = content.full;
-                        delete content.full;
-                }
-                if (content.thumb) {
-                    content.images.thumb = {};
-                        content.images.thumb.url = content.thumb;
-                        delete content.thumb;
-                }
-            }
-
-            sailthru.apiPost("content", 
-                content,
-            function(err, response) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    success_count = success_count + 1;
-                    console.log(response);
-                }
-            });
-        });
-    }
-    else if (req.body.id == "delete") {
-        let success_count = 0;
-        const all_content = req.body.data;
-        all_content.forEach(content => {
-            sailthru.apiDelete("content", {
-                url: content.url
-            },
-            function(err, response) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log(response);
-                    success_count = success_count + 1;
-                }
-            });
-        });
-        res.send(JSON.stringify({"success_count": 6}));
-    }
-    else if (req.body.id == "export") {
+    if (req.body.id == "export") {
         const data = [];
         sailthru.apiGet("content", {
             items: 1000
